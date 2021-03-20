5acbef86c87b4d33aeec63e1d04101a9c8523554
philosurfer <mariocrivello@gmail.com>
2021-03-20 14:14:50 -0700

    started install script file for macOS

---
+++ sync_to_prod.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# macOS :
+# syncs repo's files to default macOS folder locations for reason codecs
+
+cp 'Remote/Codecs/Lua/*' '/Library/Application Support/Propellerhead Software/Remote/Codecs/Lua Codecs/Akai/';
+cp 'Remote/Maps/Akai/*' '/Library/Application Support/Propellerhead Software/Remote/Codecs/';
