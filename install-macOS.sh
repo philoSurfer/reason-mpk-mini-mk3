4bbedf9cb63bf8003066db1b6dfbb1fdcf965520
shaack <shaack@gmail.com>
2023-08-28 22:29:59 +0200

    Added directory creation in macOS install script to make it working.

--- install-macOS.sh
+++ install-macOS.sh
@@ -3,16 +3,14 @@
 # macOS :
 # syncs repo's files to default macOS folder locations for reason codecs
 ## NOT TESTED YET
-echo '\n';
-echo '---COPYING--------------------------+++- ';
-echo '\n';
+echo 'COPYING...';
 
+mkdir -p ~/Library/Application\ Support/Propellerhead\ Software/Remote/Codecs/Lua\ Codecs/Akai/
 cp ./Remote/Codecs/Lua\ Codecs/Akai/*.* ~/Library/Application\ Support/Propellerhead\ Software/Remote/Codecs/Lua\ Codecs/Akai/;
 echo 'Lua Codecs copied.';
-echo '\n';
 
+mkdir -p ~/Library/Application\ Support/Propellerhead\ Software/Remote/Maps/Akai/
 cp ./Remote/Maps/Akai/*.* ~/Library/Application\ Support/Propellerhead\ Software/Remote/Maps/Akai/;
 echo 'Remote Maps copied.';
-echo '\n';
 
-echo '---COMPLETE-------------------------XXX-';
+echo '⇨ COMPLETE';
